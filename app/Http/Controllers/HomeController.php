e5b8256310138eb0d43bc597cee60291125a4d7c
amimhayden22 <amimopreker22@gmail.com>
2023-01-04 11:17:41 +0700

    feat: dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+         // Membuat line chart
+         $monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+         $monthCustom = [
+             '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'
+         ];
+
+         $getTransactions = [];
+         foreach ($monthCustom as $value) {
+             $getTransactions[] = Transaction::whereStatus('Disetujui')
+                             ->whereMonth('leave_date', $value)
+                             ->count();
+         }
+
+         return view('home')
+         ->with('monthNames',json_encode($monthNames,JSON_NUMERIC_CHECK))
+         ->with('getTransactions',json_encode($getTransactions,JSON_NUMERIC_CHECK));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,73 @@ Dasbor
             </div>
         </div>
     </div>
+    @if (Auth::user()->role === 'Administrator' || Auth::user()->role === 'HRD')
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Grafik Pengajuan Cuti Karyawan per Bulan</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="myChart1"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 </section>
+@endsection
+@section('script')
+    @if (Auth::user()->role === 'Administrator' || Auth::user()->role === 'HRD')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+            var monthNames = {!! $monthNames !!};
+            var transactions = {!! $getTransactions !!};
+
+            window.onload = function(){
+                var ctx = document.getElementById("myChart1").getContext('2d');
+                lineChartWindow = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: monthNames,
+                        datasets: [{
+                            label: 'Jumlah Karyawan Cuti',
+                            data: transactions,
+                            borderWidth: 2,
+                            backgroundColor: '#3772ff',
+                            borderColor: '#3772ff',
+                            borderWidth: 2.5,
+                            pointBackgroundColor: '#ffffff',
+                            pointRadius: 4
+                        }]
+                    },
+                    options: {
+                        legend: {
+                            display: false
+                        },
+                        scales: {
+                            yAxes: [{
+                                gridLines: {
+                                    drawBorder: false,
+                                    color: '#f2f2f2',
+                                },
+                                ticks: {
+                                    beginAtZero: true,
+                                    stepSize: 150
+                                }
+                            }],
+                            xAxes: [{
+                                ticks: {
+                                    display: false
+                                },
+                                gridLines: {
+                                    display: false
+                                }
+                            }]
+                        },
+                    }
+                });
+            }
+        </script>
+    @endif
 @endsection
